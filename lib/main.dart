ee3d24b43990f6ffddc1c406e94f3774f0a54b9c
PrQnHuBer <mokil05zaza@hotmail.com>
2025-03-05 11:14:46 +0700

    V.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:iot_mukrata_project/views/menu_ui.dart';
-
+import 'package:iot_mukrata_project/views/splash_screen_ui.dart';
 void main() {
-  runApp(IoTMukrataProject());
+  runApp(
+    IoTMukrataProject(),
+  );
 }
-
+//------------------------------------------
 class IoTMukrataProject extends StatefulWidget {
   const IoTMukrataProject({super.key});
-
-  // This widget is the root of your application.
+  @override
+  State<IoTMukrataProject> createState() => _IoTMukrataProjectState();
+}
+class _IoTMukrataProjectState extends State<IoTMukrataProject> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: SplashScreenUI(),
-        theme: ThemeData(
-          textTheme: GoogleFonts.kanitTextTheme(
-            Theme.of(context).textTheme,
-          ),
-        ));
+      debugShowCheckedModeBanner: false,
+      home: SplashScreenUI(),
+      theme: ThemeData(
+        textTheme: GoogleFonts.kanitTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+    );
   }
 }
